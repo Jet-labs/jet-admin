c099fd15b7386f9ae8cdcd30d08743f4c87b8b5d
ragsav <raghav227996399@gmail.com>
2026-04-26 10:58:56 +0530

    fix(frontend): add DataQueriesContextProvider and WidgetsContextProvider to DashboardLayout

--- apps/frontend/src/presentation/components/layouts/dashboardLayout.jsx
+++ apps/frontend/src/presentation/components/layouts/dashboardLayout.jsx
@@ -1,41 +1,43 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-// import logo from "../../../assets/logo.png";
 
 import { CONSTANTS } from "../../../constants";
 import { DashboardsContextProvider } from "../../../logic/contexts/dashboardsContext";
+import { DataQueriesContextProvider } from "../../../logic/contexts/dataQueriesContext";
+import { WidgetsContextProvider } from "../../../logic/contexts/widgetsContext";
 import { DashboardDrawerList } from "../drawerList/dashboardDrawerList";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "../ui/resizable";
-import { AIChatButton } from "../aiChatComponents/aiChatButton";
 
 export const DashboardLayout = () => {
   return (
     <DashboardsContextProvider>
-      <div className="flex h-full w-full flex-col overflow-hidden bg-background relative">
-        <ResizablePanelGroup
-          direction="horizontal"
-          autoSaveId={
-            CONSTANTS.RESIZABLE_PANEL_KEYS.DASHBOARD_LAYOUT_SEPARATION
-          }
-          className="h-full w-full"
-        >
-          <ResizablePanel defaultSize={20}>
-            <DashboardDrawerList />
-          </ResizablePanel>
-          <ResizableHandle withHandle={true} />
-          <ResizablePanel defaultSize={80} className="overflow-hidden bg-background">
-            <Outlet />
-          </ResizablePanel>
-        </ResizablePanelGroup>
-
-        {/* Global Agent Chat UI */}
-        {/* <AIChatButton /> */}
-      </div>
+      <DataQueriesContextProvider>
+        <WidgetsContextProvider>
+          <div className="flex h-full w-full flex-col overflow-hidden bg-white relative">
+            <ResizablePanelGroup
+              direction="horizontal"
+              autoSaveId={
+                CONSTANTS.RESIZABLE_PANEL_KEYS.DASHBOARD_LAYOUT_SEPARATION
+              }
+              className="h-full w-full"
+            >
+              <ResizablePanel defaultSize={20}>
+                <DashboardDrawerList />
+              </ResizablePanel>
+              <ResizableHandle withHandle={true} />
+              <ResizablePanel defaultSize={80} className="overflow-hidden bg-white">
+                <Outlet />
+              </ResizablePanel>
+            </ResizablePanelGroup>
+          </div>
+        </WidgetsContextProvider>
+      </DataQueriesContextProvider>
     </DashboardsContextProvider>
   );
 };
+
 
